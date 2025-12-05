0f8be335a01238ff498b16e0626f2169f2917fbc
yoon-byeongil <justbrand101@gmail.com>
2025-12-05 10:59:30 +0900

    fix gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,14 +1,17 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-def keystoreProperties = new Properties()
-def keystorePropertiesFile = rootProject.file('key.properties')
+// [수정] Kotlin 문법으로 변경된 키스토어 로드 로직
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
 if (keystorePropertiesFile.exists()) {
-    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 
 android {
@@ -27,36 +30,31 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.y101.supersimpletodo"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    // ▼▼▼ [추가된 부분] 서명 설정 (Signing Configs) ▼▼▼
+    // [수정] Kotlin 문법으로 변경된 서명 설정
     signingConfigs {
-        release {
-            keyAlias = keystoreProperties['keyAlias']
-            keyPassword = keystoreProperties['keyPassword']
-            storeFile = keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
-            storePassword = keystoreProperties['storePassword']
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = if (keystoreProperties["storeFile"] != null) file(keystoreProperties["storeFile"] as String) else null
+            storePassword = keystoreProperties["storePassword"] as String
         }
     }
-    // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
     buildTypes {
         release {
-            // ▼▼▼ [수정된 부분] debug 대신 release 서명 사용 ▼▼▼
-            signingConfig = signingConfigs.release
-            // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
+            // [수정] Kotlin 문법으로 변경된 release 설정
+            signingConfig = signingConfigs.getByName("release")
             
-            // (선택 사항) 코드 난독화 및 리소스 축소 (오류 나면 false로 바꾸세요)
-            minifyEnabled false
-            shrinkResources false
+            // 코드 난독화 설정 (Kotlin에서는 isMinifyEnabled로 씁니다)
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
